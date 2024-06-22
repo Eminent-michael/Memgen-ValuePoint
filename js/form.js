f76d4c49a39290d830dddeb8394b5519b15af9ec
Eminent-michael <michaelafolab@gmail.com>
2024-06-22 04:00:14 +0100

    quick form working

--- js/form.js
+++ js/form.js
@@ -1,33 +1,52 @@
 /** @format */
 
-// Variables
+// Show form
 
-// const showForm = document.querySelector("#show-form")
-// const onboardForm = document.querySelector("#Onboard-form")
+const showBtn = document.getElementById("show-form");
 
-// const onboardData = document.querySelector("#onboard-data")
+showBtn.addEventListener("click", () => {
+  document.getElementById("Onboard-form").classList.remove("hidden")
+})
 
-// Show Form
-// showForm.addEventListener("click", () => {
-//     onboardForm.classList.remove("hidden")
-// })
 
-// Send Form data
+// Drop Down Effect on form page
 
-// function sendData() {
+const digitalAsset = document.querySelector("#digital-asset");
+const digitalAssetBody = document.querySelector("#digital-asset-body");
+const dropdownIcon = document.querySelector("#digital-dropdown");
 
-// }
+const estateAsset = document.querySelector("#estate-asset");
+const estateAssetBody = document.querySelector("#estate-asset-body");
+const estateDropdownIcon = document.querySelector("#estate-dropdown");
 
-// Form submission
+const tradingAsset = document.querySelector("#trading-asset");
+const tradingAssetBody = document.querySelector("#trading-asset-body");
+const tradingDropdownIcon = document.querySelector("#trading-dropdown");
 
-// onboardData.addEventListener("submit", (event) => {
-//     event.preventDefault();
+digitalAsset.addEventListener("click", () => {
+  digitalAssetBody.classList.toggle("addheight");
+  digitalAssetBody.classList.toggle("py-8");
+  dropdownIcon.classList.toggle("open");
+});
 
-// })
+estateAsset.addEventListener("click", () => {
+  estateAssetBody.classList.toggle("addheight");
+  estateAssetBody.classList.toggle("py-8");
+  estateDropdownIcon.classList.toggle("open");
+});
+
+tradingAsset.addEventListener("click", () => {
+  tradingAssetBody.classList.toggle("addheight");
+  tradingAssetBody.classList.toggle("py-8");
+  tradingDropdownIcon.classList.toggle("open");
+});
+
 
 let currentStage = 0;
 const stages = document.querySelectorAll(".stage");
 const nextButton = document.getElementById("nextBtn");
+const nextBtnText = document.querySelector(".nextBtnText");
+const nextIcon = document.querySelector(".next-icon");
 const prevButton = document.getElementById("prevBtn");
 
 function showStage(stage) {
@@ -35,6 +54,15 @@ function showStage(stage) {
     s.style.display = index === stage ? "block" : "none";
   });
   prevButton.classList.toggle("invisible", stage === 0);
+
+  // Update the next button text based on the stage
+  if (stage === stages.length - 1) {
+    nextBtnText.textContent = "Submit";
+    nextIcon.classList.add("hidden");
+  } else {
+    nextBtnText.textContent = "Next";
+    nextIcon.classList.remove("hidden");
+  }
 }
 
 function validateForm() {
@@ -53,7 +81,8 @@ nextButton.addEventListener("click", (event) => {
   if (validateForm()) {
     currentStage++;
     if (currentStage >= stages.length) {
-      alert("Form completed!");
+      checkServiceCheckbox();
+      // alert("Form completed!");
       // Optionally handle form submission here
     } else {
       showStage(currentStage);
@@ -71,4 +100,106 @@ prevButton.addEventListener("click", (event) => {
 });
 
 showStage(currentStage); // Show the first stage
+
+// Check if any service has been checked
+
+function checkServiceCheckbox() {
+  const checkboxes = document.querySelectorAll(".service-checkbox");
+  const anyChecked = Array.from(checkboxes).some(
+    (checkbox) => checkbox.checked
+  );
+
+  if (anyChecked) {
+    collectFormData();
+  } else {
+    alert("No checkboxes are checked.");
+  }
+}
+
+// Function to collect user data from the form
+function collectUserData() {
+  const userData = {
+    fullName: document.querySelector("#onBoardFullName").value,
+    email: document.querySelector("#onBoardEmail").value,
+    tel: document.querySelector("#onBoardTel").value,
+    address: document.querySelector("#onBoardAddress").value,
+    city: document.querySelector("#onBoardCity").value,
+    state: document.querySelector("#onBoardState").value,
+    country: document.querySelector("#onBoardCountry").value,
+    contactMethod: document.querySelector("#onBoardPreCont").value,
+  };
+
+  return userData;
+}
+
+// Function to collect service interest data
+function collectServiceInterest(
+  serviceCheckboxId,
+  expSelector,
+  groupClassName
+) {
+  const serviceData = {};
+
+  if (document.querySelector(serviceCheckboxId).checked) {
+    const experience = document.querySelector(expSelector).value;
+
+    serviceData.experience = experience;
+
+    const serviceGroups = document.querySelectorAll(groupClassName);
+
+    serviceGroups.forEach((group) => {
+      const groupName = group.getAttribute("data-group");
+
+      const checkedBoxes = group.querySelectorAll(
+        `input[name=${groupName}]:checked`
+      );
+
+      const textInput = group.querySelectorAll(`input[name=${groupName}Text]`);
+
+      serviceData[groupName] = {
+        checkboxes: Array.from(checkedBoxes).map((box) => box.value),
+        // textInput: textInput ? textInput.value : "",
+        textInput: Array.from(textInput).map((serText) => serText.value),
+      };
+
+      console.log(
+        `Service data for group ${groupName}:`,
+        serviceData[groupName]
+      );
+    });
+  } else {
+    console.log(`${serviceCheckboxId} Not Selected`);
+  }
+
+  return serviceData;
+}
+
+// Main function to collect all form data
+function collectFormData() {
+  const formData = {};
+  formData.user = collectUserData();
+  console.log("User Data:", formData.user);
+
+  formData.interest = {};
+
+  formData.interest.digitalAssetService = collectServiceInterest(
+    "#digitalAssetService",
+    "#digitalExp",
+    ".digital-service-group"
+  );
+
+  formData.interest.forestTradingService = collectServiceInterest(
+    "#forestTradingService",
+    "#forexExp",
+    ".forex-trading-group"
+  );
+
+  formData.interest.realEstateService = collectServiceInterest(
+    "#realEstateService",
+    "#estate-exp",
+    ".real-estate-group"
+  );
+
+  console.log("Form Data:", formData);
+}
 
---
+++ js/quick-form.js
@@ -0,0 +1,36 @@
+/** @format */
+
+// Quick Form
+
+const form = document.getElementById("quickForm");
+const submitButton = document.getElementById("quickBtn");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // Prevent the form from submitting the default way
+
+  // Collect form data
+  const quickFormData = {
+    fullName: document.getElementById("quickFullName").value,
+    email: document.getElementById("quickEmail").value,
+    tel: document.getElementById("quickTel").value,
+    interest: document.getElementById("quickInterest").value,
+  };
+
+  // Format the message for WhatsApp
+  const message = `Full Name: ${quickFormData.fullName}%0AEmail: ${quickFormData.email}%0APhone No: ${quickFormData.tel}%0AService Interest: ${quickFormData.interest}`;
+
+  // WhatsApp number in international format without '+' (e.g., 2348024488521)
+  const whatsappNumber = "2348024488521";
+
+  // WhatsApp API link
+  const whatsappApiLink = `https://wa.me/${whatsappNumber}?text=${message}`;
+
+  // Redirect to WhatsApp
+  window.open(whatsappApiLink, "_blank");
+
+  // Optionally reset the form fields
+  form.reset();
+
+  // Display form data in console (for demonstration)
+  console.log("Form Data:", quickFormData);
+});
